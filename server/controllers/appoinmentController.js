1444b5bdba07b216cae112e85adbf26663402ac9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-07-03 12:01:47 +0530

    Update appoinmentController.js

--- server/controllers/appoinmentController.js
+++ server/controllers/appoinmentController.js
@@ -24,14 +24,84 @@ const AppoinmentController = {
 
             const resultAppoinment = await newAppoinment.save()
 
-            if(resultAppoinment){
-                return res.json({ Status: "Success", Message: "Appoinment Created Success"})
+            if (resultAppoinment) {
+                return res.json({ Status: "Success", Message: "Appoinment Created Success" })
             }
-            else{
-                return res.json({ Error: "Internal Server Error"})
+            else {
+                return res.json({ Error: "Internal Server Error" })
             }
         }
         catch (err) {
+            console.log(err)
+        }
+    },
+
+    getallappoiments: async (req, res) => {
+        try {
+            const allappoinemtns = await Appoinment.find()
+
+            return res.json({ Result: allappoinemtns })
+        }
+        catch (err) {
+            console.log(err)
+        }
+    },
+
+    getoneappoinment: async (req, res) => {
+        try {
+            const id = req.params.id
+
+            const getappoimentbyid = await Appoinment.findById(id)
+
+            return res.json({ Result: getappoimentbyid })
+        }
+        catch (err) {
+            console.log(err)
+        }
+    },
+
+    getmyappoinments: async (req, res) => {
+        try {
+            const token = req.header('Authorization');
+            if (!token || !token.startsWith('Bearer ')) {
+                return res.json({ Error: "Missing or invalid token" });
+            }
+
+            const decoded = jwt.verify(token.replace('Bearer ', ''), process.env.JWT_SECRET);
+            const tokenID = decoded.id;
+
+            const myappoinemts = await Appoinment.find({ userID: tokenID })
+
+            if(myappoinemts.length  === 0){
+                return res.json({ Error: "No Appoiments Found"})
+            }
+
+            return res.json({ Result: myappoinemts })
+        }
+        catch (err) {
+            console.log(err)
+        }
+    },
+
+    doctorappoinmets: async(req, res) => {
+        try{
+            const token = req.header('Authorization');
+            if (!token || !token.startsWith('Bearer ')) {
+                return res.json({ Error: "Missing or invalid token" });
+            }
+
+            const decoded = jwt.verify(token.replace('Bearer ', ''), process.env.JWT_SECRET);
+            const tokenID = decoded.id;
+            
+            const appoimentdoctor = await Appoinment.find({ doctorID: tokenID })
+
+            if(appoimentdoctor.length === 0){
+                return res.json({ Error: "No Appoinments found"})
+            }
+            
+            return res.json({ Result: appoimentdoctor})
+        }
+        catch(err){
             console.log(err)
         }
     }
